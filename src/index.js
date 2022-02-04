134094dd750b943a4c604aa5e3ffd86a1f0679a7
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-02-04 17:48:46 -0500

    Mensajes aleatorios

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+    "Julian",
+    "Ana",
+    "Nicola",
+    "Diego",
+    "Laura"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+};
+
+module.exports = { randomMsg }
